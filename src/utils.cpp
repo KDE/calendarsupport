fc1d5df11794c29f96b87329a78d163443445817
Montel Laurent <montel@kde.org>
2016-03-17 08:04:39 +0100

    Astyle kdelibs

--- src/utils.cpp
+++ src/utils.cpp
@@ -570,12 +570,12 @@ QString CalendarSupport::displayName(Akonadi::ETMCalendar *calendar, const Akona
         }
 
         if (!ownerStr.isEmpty()) {
-            if (!ownerStr.compare( QLatin1String( "INBOX" ), Qt::CaseInsensitive )) {
+            if (!ownerStr.compare(QLatin1String("INBOX"), Qt::CaseInsensitive)) {
                 return i18nc("%1 is folder contents",
                              "My Kolab %1", typeStr);
-            } else if ( !ownerStr.compare( QLatin1String( "SHARED" ), Qt::CaseInsensitive) ||
-                        !ownerStr.compare( QLatin1String( "CALENDAR"), Qt::CaseInsensitive) ||
-                        !ownerStr.compare( QLatin1String( "RESOURCES"), Qt::CaseInsensitive)) {
+            } else if (!ownerStr.compare(QLatin1String("SHARED"), Qt::CaseInsensitive) ||
+                       !ownerStr.compare(QLatin1String("CALENDAR"), Qt::CaseInsensitive) ||
+                       !ownerStr.compare(QLatin1String("RESOURCES"), Qt::CaseInsensitive)) {
                 return i18nc("%1 is folder name, %2 is folder contents",
                              "Shared Kolab %1 %2", nameStr, typeStr);
             } else {
