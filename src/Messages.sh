59240a42834bb9ce77ff17eee7d7e14a2c93b537
Laurent Montel <montel@kde.org>
2021-12-07 07:05:04 +0100

    GIT_SILENT: remove some reuse lint warning

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $EXTRACTRC `find . -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/calendarsupport.pot
 rm -f rc.cpp
