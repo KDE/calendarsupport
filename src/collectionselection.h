3828054c0901841cc880bad341450f9ceece15f6
Allen Winter <winter@kde.org>
2026-03-22 07:28:22 -0400

    src/collectionselection.[cpp,h] - fix the KDAB copyright line
    use proper company name and email

--- src/collectionselection.h
+++ src/collectionselection.h
@@ -1,5 +1,5 @@
 /*
-  SPDX-FileCopyrightText: 2009 KDAB
+  SPDX-FileCopyrightText: 2009 Klaralvdalens Datakonsult AB, a KDAB Group company <info@kdab.net>
   SPDX-FileContributor: Frank Osterfeld <frank@kdab.net>
 
   SPDX-License-Identifier: GPL-2.0-or-later WITH LicenseRef-Qt-Commercial-exception-1.0
